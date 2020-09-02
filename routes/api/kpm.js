77ea586feb19038e65102f56ec0362d3bac92656
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-09-02 10:25:05 +0700

    Add get one kpm

--- routes/api/kpm.js
+++ routes/api/kpm.js
@@ -4,9 +4,9 @@ const Kpm = require('../../models/Kpm')
 
 const router = express.Router();
 
-// @route POST api/kpm
-// @desc Register new KPM
-// @access Public
+// @route   POST api/kpm
+// @desc    Register new KPM
+// @access  Public
 router.post('/', async (req, res) => {
     const {
         kks,
@@ -32,11 +32,37 @@ router.post('/', async (req, res) => {
 
         // Return kpm data
         return res.json({
-            user
+            kpm
         })
     } catch (err) {
         console.log(err.message)
         req.status(500).send('Server error')
+    }
+})
+
+// @route   GET api/kpm/getOne
+// @desc    Get KPM with kks
+// @access  Public
+router.get('/:kks', async (req, res) => {
+    try {
+        let kks = req.params.kks;
+        // Check if user exists
+        let kpm = await Kpm.findOne({
+            kks
+        })
+
+        if (!kpm) {
+            return res.json({
+                kpm: []
+            })
+        }
+
+        return res.json({
+            kpm
+        })
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send('Server error')
     }
 })
 
