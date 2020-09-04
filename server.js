64c4c3b9bd7902f755b57d965a4ca50beda2fe04
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-09-04 10:02:13 +0700

    Fix dispatching OPTIONS

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
-const express = require('express')
-const connectDB = require('./config/db')
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+app.options("*", cors(corsOptions));
 
 // Connect db
 connectDB();
 
 // Init Middleware
-app.use(express.json({
-    extended: false
-}))
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Request-Headers', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Request-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
 });
 
 // Define Routes
-app.use('/api/kpm', require('./routes/api/kpm'))
-app.use('/api/goods', require('./routes/api/goods'))
+app.use("/api/kpm", require("./routes/api/kpm"));
+app.use("/api/goods", require("./routes/api/goods"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-})
+  console.log(`Server started on port ${PORT}`);
+});
